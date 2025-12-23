fcf6dc210a816235870ab67eac45ceb0747a986b
Chanrith Nhen <nhenchanrithofficial@gmail.com>
2025-12-23 11:09:46 +0700

    first-commit-index-style-script-todo-app

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,1 +1,61 @@
-console.log("Hello, World!");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const completedCounter = document.getElementById("completed-counter");
+const uncompletedCounter = document.getElementById("uncompleted-counter");
+
+function addTask() {
+    const task = inputBox.value.trim();
+    if (!task) {
+        alert("Please write down a task!");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <label>
+        <input type="checkbox" />
+        <span>${task}</span>
+    </label>
+    <span class="delete-btn">Delete</span>
+    <span class="edit-btn">Edit</span>
+    `;
+
+    const checkbox = li.querySelector("input");
+    const editBtn = li.querySelector(".edit-btn");
+    const deleteBtn = li.querySelector(".delete-btn");
+    const taskSpan = li.querySelector("span");
+
+    checkbox.addEventListener("click", function () {
+        li.classList.toggle("completed", checkbox.checked);
+        updateCounters();
+    });
+
+    editBtn.addEventListener("click", function () {
+        const update = prompt("Edit task:", taskSpan.textContent);
+        if (update !== null) {
+            taskSpan.textContent = update;
+            li.classList.remove("completed");
+
+            checkbox.checked = false;
+            updateCounters();
+        }
+    });
+
+    deleteBtn.addEventListener("click", function () {
+        if (confirm("Are you sure you want to delete this task?")) {
+            li.remove();
+            updateCounters();
+        }
+    })
+
+    listContainer.appendChild(li);
+    inputBox.value = "";
+}
+
+function updateCounters() {
+    const completedTasks = document.querySelectorAll(".completed").length;
+    const uncompletedTasks = document.querySelectorAll("li:not(.completed)").length;
+
+    completedCounter.textContent = completedTasks;
+    uncompletedCounter.textContent = uncompletedTasks;
+}
